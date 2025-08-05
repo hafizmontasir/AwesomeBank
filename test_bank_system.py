bb3eff21c7f784d8d5500d2052eb3aa31c46e0fb
MD Hafiz Montasir Mishan <mishanhafiz23@gmail.com>
2025-08-05 18:44:49 +0800

    Add TestTransaction class to test Transaction

--- test_bank_system.py
+++ test_bank_system.py
@@ -6,7 +6,26 @@ from bank_system import BankingSystem, Account, Transaction, InterestRule
 
 class TestTransaction(unittest.TestCase):
     """Test Transaction class"""
-    pass
+    
+    def test_transaction_creation(self):
+        """Test transaction object creation"""
+        txn = Transaction("20230626", "AC001", "D", Decimal('100.00'), "20230626-01")
+        self.assertEqual(txn.date, "20230626")
+        self.assertEqual(txn.account, "AC001")
+        self.assertEqual(txn.txn_type, "D")
+        self.assertEqual(txn.amount, Decimal("100.00"))
+        self.assertEqual(txn.id, "20230626-01")
+    
+    def test_transaction_type_case_insensitive(self):
+        """Test Transaction type is converted to uppercase"""
+        txn = Transaction("20230626", "AC001", "d", Decimal('100.00'), "20230626-01")
+        self.assertEqual(txn.txn_type, "D")
+        
+        txn2 = Transaction("20230626", "AC001", "w", Decimal('100.00'), "20230626-01")
+        self.assertEqual(txn2.txn_type, "W")
+    
+    
+
 
 class TestInterestRule(unittest.TestCase):
     """Test InterestRule class"""
