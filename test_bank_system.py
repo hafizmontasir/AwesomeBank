c2d2cd975b5ba5f39aab524458da2f026a426d5e
MD Hafiz Montasir Mishan <mishanhafiz23@gmail.com>
2025-08-05 17:11:50 +0800

    Define initial test structure for bank system

---
+++ test_bank_system.py
@@ -0,0 +1,28 @@
+"""Unit tests for the AwesomeGIC Banking System"""
+
+import unittest
+from decimal import Decimal
+from bank_system import BankingSystem, Account, Transaction, InterestRule
+
+class TestTransaction(unittest.TestCase):
+    """Test Transaction class"""
+    pass
+
+class TestInterestRule(unittest.TestCase):
+    """Test InterestRule class"""
+    pass
+
+class TestAccount(unittest.TestCase):
+    """Test Account class"""
+    pass
+
+class TestBankingSystem(unittest.TestCase):
+    """Test BankingSystem class"""
+    pass
+
+class TestIntegration(unittest.TestCase):
+    """Integration Tests"""
+    pass
+
+if __name__=='__main__':
+    unittest.main(verbosity=2)
